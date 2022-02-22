0f84b7a05d4ebb9529424356eed4c9db25ae4102
yeonha99 <rhdqn1207@naver.com>
2022-02-22 18:05:20 +0900

    feat:Web config

---
+++ src/main/java/ice/comento/config/WebSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package ice.comento.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity)
+            throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .and()
+                .csrf().ignoringAntMatchers("/h2-console/**").disable();
+
+    }
+}
