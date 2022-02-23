b3b78399d1823efbef5c714eac0bd0771e1b30fa
young2yam <0v0e@inha.edu>
2022-02-24 01:25:37 +0900

    add: [Board] 댓글 @document 어노테이션 추가

--- src/main/java/ice/comento/board/domain/Comment.java
+++ src/main/java/ice/comento/board/domain/Comment.java
@@ -3,14 +3,19 @@ package ice.comento.board.domain;
 import ice.comento.model.BaseEntity;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * 글에 대한 댓글
  *
  * @author baki90
  */
+@Document
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment extends BaseEntity {
+    @Id
+    private String id;
     private String userId; // 글쓴이 아이디
     private String boardId; // 답변 단 게시글 ID
     private String commentId; // 답변 단 댓글 ID
