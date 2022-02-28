f8e7ff0ccb83f7c432f99e495866a0ecb783cea9
young2yam <0v0e@inha.edu>
2022-02-28 22:41:32 +0900

    fix: embeddable 시 Collection 어노테이션 추가

--- src/main/java/ice/comento/board/domain/HashTags.java
+++ src/main/java/ice/comento/board/domain/HashTags.java
@@ -1,9 +1,16 @@
 package ice.comento.board.domain;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
 import java.util.List;
 
 @Embeddable
 public class HashTags {
+
+    @ElementCollection
+    @CollectionTable(name = "tag",
+            joinColumns = @JoinColumn(name = "board_id"))
     private List<String> hashTags;
 }
--- src/main/java/ice/comento/board/domain/Photos.java
+++ src/main/java/ice/comento/board/domain/Photos.java
@@ -1,9 +1,15 @@
 package ice.comento.board.domain;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
 import java.util.List;
 
 @Embeddable
 public class Photos {
+    @ElementCollection
+    @CollectionTable(name = "photo",
+            joinColumns = @JoinColumn(name = "board_id"))
     private List<String> photos;
 }
