05d8375c24c783ef1cfc50e8383a0411e80ebbf8
yeonha99 <rhdqn1207@naver.com>
2022-03-14 21:14:01 +0900

    fix:정통인의 재학 상태를 영어로 접근하여 title로 한글 리턴 받을 수 있게 변경

--- src/main/java/ice/comento/user/Status.java
+++ src/main/java/ice/comento/user/Status.java
@@ -1,5 +1,21 @@
 package ice.comento.user;
 
+/**
+ * @// FIXME: 2022/03/14 영어와 한글 매핑 가능하도록 수정
+ * @author jeong-yeonhee
+ */
 public enum Status {
-    재학,휴학,졸업
+    STUDENT("재학"),
+    LEAVE("휴학"),
+    GRADUATE("졸업");
+
+    private String title;
+
+    Status(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
