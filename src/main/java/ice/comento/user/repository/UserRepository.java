ca0cb10ca45b1c19f4a5ffa8d4843c992270256c
young2yam <0v0e@inha.edu>
2022-03-16 23:30:36 +0900

    add: User Repository 생성

--- src/main/java/ice/comento/user/repository/UserRepository.java
+++ src/main/java/ice/comento/user/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package ice.comento.user.repository;public interface UserRepository {
+package ice.comento.user.repository;
+
+import ice.comento.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository<T extends User> extends JpaRepository<T, String> {
+
 }
