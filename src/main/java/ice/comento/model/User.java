750ce4c5bb07f39560cf78dd6b41f27a0e0c6f22
young2yam <0v0e@inha.edu>
2022-03-16 23:22:46 +0900

    feat: 회원 CUD 함수 구현

--- src/main/java/ice/comento/model/User.java
+++ src/main/java/ice/comento/model/User.java
@@ -1,5 +1,10 @@
 package ice.comento.model;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 /**
  * @author jeong-yeonhee
@@ -8,6 +13,7 @@ import javax.persistence.*;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="role")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 abstract public class User extends BaseEntity {
     protected String logId;
     protected String pw;
@@ -15,4 +21,47 @@ abstract public class User extends BaseEntity {
     protected String nickName;
     protected String introduction;
     protected Rank rank;
+    protected UserStatus userStatus;
+
+    @Builder
+    protected User(String logId, String pw, int studentId, String nickName, String introduction, Rank rank) {
+        this.logId = logId;
+        this.setPw(pw);
+        this.setNickName(nickName);
+        this.studentId = studentId;
+        this.introduction = introduction;
+        this.rank = rank;
+        this.userStatus = UserStatus.ACTIVE;
+    }
+
+    private void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    private void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    /**
+     * User 정보를 수정한다.
+     * @author baki90
+     * @param pw
+     * @param nickName
+     * @param introduction
+     * @param rank
+     */
+    protected void updateUser(String pw, String nickName, String introduction, Rank rank){
+        if(pw != null) this.setPw(pw);
+        if(nickName != null) setNickName(nickName);
+        if(introduction != null) this.introduction = introduction;
+        if(rank != null) this.rank = rank;
+    }
+
+    /**
+     * 회원 탈퇴
+     */
+    public void deleteUser() {
+        this.userStatus = UserStatus.WITHDRAW;
+    }
+
 }
--- src/main/java/ice/comento/user/Worker.java
+++ src/main/java/ice/comento/user/Worker.java
@@ -1,12 +1,19 @@
 package ice.comento.user;
 
+import ice.comento.board.domain.Comment;
+import ice.comento.model.Rank;
 import ice.comento.model.User;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @author jeong-yeonhee
+ * 현직자 정보 entity
+ * @author baki90
  */
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -14,16 +21,49 @@ public class Worker extends User {
 
     private String company;
 
-   @ManyToOne
-   @JoinColumn(name="job_id")
-   private Job job;
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Job job;
+    private double adoptionRate; // 답변 채택률
 
+    @Builder
+    public Worker(String logId, String pw, int studentId, String nickName, String introduction, Rank rank, String company, Job job, double adoptionRate) {
+        super(logId, pw, studentId, nickName, introduction, rank);
+        this.company = company;
+        this.job = job;
+        this.adoptionRate = 0;
+    }
 
-    /* @OneToMany(mappedBy = "answer_id",fetch = FetchType.LAZY)
-    private List<Answer> answerList=new ArrayList<>();
-    Answer 엔티티 만들어지면 연결
-    근데 게시글 엔티티도 있어야 하는 것 아닌가에 대한 의문
-    */
-    private double adoptionRate;
+    /**
+     * 현직자 정보를 수정한다.
+     * @param pw
+     * @param nickName
+     * @param introduction
+     * @param rank
+     * @param job
+     */
+    public void updateWorker(String pw, String nickName, String introduction, Rank rank, Job job) {
+        this.updateUser(pw, nickName, introduction, rank);
+        this.job = job;
+    }
 
+    /**
+     * 답변 채택률을 세팅한다.
+     * @param adoptionRate
+     */
+    public void setAdoptionRate(double adoptionRate) {
+        if(adoptionRate >= 0) {
+            this.adoptionRate = adoptionRate;
+        }
+    }
+
+    /**
+     * 답변 채택률을 계산한다.
+     * @param adoptionCount 채택 개수
+     * @param commentCount 답변 개수
+     * @return 답변 채택률
+     */
+    public double calcAdoptionRate(int adoptionCount, int commentCount) {
+        return adoptionCount / (double) commentCount;
+    }
 }
